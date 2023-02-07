943b2868966e3004780c284c9d0699d091814577
Petrik diak <diak@petrik.hu>
2023-02-07 10:42:13 +0100

    sikeres boostrap

--- src/components/Navbar.1.js
+++ src/components/Navbar.1.js
@@ -2,7 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import logo from ".../logo.png";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 
  const Navbar = () => {
     return (
@@ -17,13 +16,13 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">Rólam <span class="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">Munkáim</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
+                        <a className="nav-link disabled" href="#"></a>
                     </li>
                 </ul>
             </div>
